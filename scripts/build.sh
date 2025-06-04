530220f06c49572e7c777ba9a05d0c58412e6d01
Finn <eqicdesign@gmail.com>
2025-06-04 08:48:26 -0600

    Init

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+# If Packages aren't installed, install them.
+if [ ! -d "Packages" ]; then
+    sh scripts/install-packages.sh
+fi
+
+rojo sourcemap default.project.json -o sourcemap.json
+& wally-package-types --sourcemap sourcemap.json Packages/
+
+ROBLOX_DEV=false darklua process --config .darklua.json src/ dist/
+rojo build build.project.json -o CermamicsGame.rbxl
